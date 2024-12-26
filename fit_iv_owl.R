6e4cdd40572e1c0c334c9c8a5281ef6ec4d50428
cuongpham1995 <cuongtuanpham@gmail.com>
2024-12-26 10:21:35 -0500

    fitting the owl under the unmeasured confounders exist assumption

---
+++ fit_iv_owl.R
@@ -0,0 +1,6 @@
+fit.iv.owl = function(itr_formula, iv_formula, trt_formula, mean_formula = NULL,method, trt, outcome, dat, 
+                      na.rm = T, minimize = F, center.outcome = F, 
+                      kernel = "linear", cross = 10, scale = T, seed, ...){ 
+  
+  
+  }
